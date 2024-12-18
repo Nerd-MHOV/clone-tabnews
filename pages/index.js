749e1baa1ac1c7f57c0684a6bb092976ffdabb5c
Matheus Henrique <84547045+Nerd-MHOV@users.noreply.github.com>
2024-12-18 18:13:45 -0300

    \add file `.editorconfig`

--- pages/index.js
+++ pages/index.js
@@ -2,4 +2,8 @@ function Home() {
     return <div>Feliz Aniversário Dayane, só mandei para dizer que te amo demais!</div>
 }
 
+function teste() {
+  console.log( 'ident' )
+}
+
 export default Home
