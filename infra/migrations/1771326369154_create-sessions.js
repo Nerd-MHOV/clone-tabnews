1a67e523bcaee503b97738224c5f878c440bd268
Matheus Henrique <matheus.henrique4245@gmail.com>
2026-02-17 09:25:30 -0300

    feat: implement session creation

---
+++ infra/migrations/1771326369154_create-sessions.js
@@ -0,0 +1,43 @@
+exports.up = (pgm) => {
+  pgm.createTable("sessions", {
+    id: {
+      type: "uuid",
+      primaryKey: true,
+      default: pgm.func("gen_random_uuid()"),
+    },
+
+    token: {
+      type: "varchar(96)",
+      notNull: true,
+      unique: true,
+    },
+
+    user_id: {
+      type: "uuid",
+      notNull: true,
+      // testando aqui, sem criar a foreign key
+      // assim como o github, algums pessoas evitam FK por performance.
+      // references: "users", // users.id
+      // onDelete: "cascade",
+    },
+
+    expires_at: {
+      type: "timestamptz",
+      notNull: true,
+    },
+
+    created_at: {
+      type: "timestamptz",
+      notNull: true,
+      default: pgm.func("timezone('utc', now())"),
+    },
+
+    updated_at: {
+      type: "timestamptz",
+      notNull: true,
+      default: pgm.func("timezone('utc', now())"),
+    },
+  });
+};
+
+exports.down = false;
